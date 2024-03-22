dbdc8fbe258ab61db091c2c207b5ddbbb939ca4e
VirLg <19virlg@gmail.com>
2024-03-22 13:49:58 +0200

    add link om imge

---
+++ js/data.js
@@ -0,0 +1,7 @@
+// import abc from '../images/BG.jpg';
+// import a from '../images/webp/'
+export const data = [
+  {
+    img: '../images/BG.jpg',
+  },
+];
--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
-console.log(5);
-console.log(5);
+import { data } from './data.js';
+// // import a from '../images/webp/mobile/shop';
+console.log('first', data);
+const ref = {
+  shopDiv: document.querySelector('.js-shop-item'),
+};
+// console.log('data', data);
+const arr = [1, 2, 3];
+const markup = data
+  .map(
+    ({ img }) =>
+      `<div>
+//  console.log('img', ${img})
+// <img src='${img}' alt=""></div>
+`
+  )
+  .join('');
+
+ref.shopDiv.insertAdjacentHTML('beforeend', markup);
