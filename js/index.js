ed210721141df0fe85d20165e2e010d69e403238
VirLg <19virlg@gmail.com>
2024-03-25 17:15:07 +0200

    modal

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 import { data, comments } from './data.js';
-
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
 const ref = {
   shopDiv: document.querySelector('.js-shop-item'),
   commentsDiv: document.querySelector('.js-comments-item'),
+  openModalBtn: document.querySelector('.hero-button'),
+  spoylerOne: document.querySelector('.js-children'),
+  modal: document.querySelector('.modal'),
+  backdrop: document.querySelector('.backdrop'),
 };
-console.log('data', ref.commentsDiv);
+
+const toggleModal = () => {
+  ref.backdrop.style.width = screenWidth + 'px';
+  ref.backdrop.style.height = screenHeight + 'px';
+  ref.modal.classList.add('is-hidden');
+  ref.backdrop.classList.add('is-hidden');
+};
+
+ref.openModalBtn.addEventListener('click', toggleModal);
 
 const markup = data
   .map(
@@ -25,8 +38,6 @@ const markup = data
   )
   .join('');
 
-ref.shopDiv.insertAdjacentHTML('beforeend', markup);
-
 const markupComments = comments
   .map(
     ({ comment, title }) =>
@@ -37,5 +48,17 @@ const markupComments = comments
     </div>`
   )
   .join('');
+const spoylerOne = () => `
+<div class="spoylerDiv-one"> 
+ <div class="spoyler-one">
+  <p >আমাদের পৃষ্ঠাটি আপনার অভিজ্ঞতা উন্নত করতে কুকিজ ব্যবহার করে। আপনি চাইলে তাদের নিষ্ক্রিয় করতে পারেন, কিন্তু এটি ওয়েবসাইটের কিছু কার্যক্ষমতা সীমিত করতে পারে। আমাদের কুকি নীতি দেখুন।</p>
+  <button class="hero-button spoyler-one">সব কুকিজকে গ্রহণ করুন</button>
+  <button class="hero-button spoyler-one">কুকিজের সেটিংস</button>
+</div>
+</div>
+  `;
 
+ref.spoylerOne.insertAdjacentHTML('afterbegin', spoylerOne);
+
+ref.shopDiv.insertAdjacentHTML('beforeend', markup);
 ref.commentsDiv.insertAdjacentHTML('beforeend', markupComments);
