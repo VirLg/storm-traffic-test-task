5487c5b238cf93d43bfaab5fa7a7b32723913138
VirLg <19virlg@gmail.com>
2024-03-25 12:20:36 +0200

    fix

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 export const data = [
   {
-    img: './images/webp/mobile/shop/bga.webp',
+    img: './images/webp/mobile/shop/bgb.webp',
     price: 'BDT 3439',
     title: 'দিগন্তীয় সামগ্রী',
   },
