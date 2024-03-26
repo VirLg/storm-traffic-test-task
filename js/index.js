bfb9ef4df7f8b55cfd360ec32a3c88ea67e8949e
VirLg <19virlg@gmail.com>
2024-03-26 15:23:11 +0200

    add

--- js/index.js
+++ js/index.js
@@ -5,33 +5,33 @@ const ref = {
   shopDiv: document.querySelector('.js-shop-item'),
   commentsDiv: document.querySelector('.js-comments-item'),
   openModalBtn: document.querySelector('.hero-button'),
+  closeModalBtn: document.querySelector('.none'),
   spoylerOne: document.querySelector('.js-children'),
   modal: document.querySelector('.modal'),
   backdrop: document.querySelector('.backdrop'),
 };
 
 const toggleModal = () => {
-  ref.backdrop.style.width = screenWidth + 'px';
-  ref.backdrop.style.height = screenHeight + 'px';
-  ref.modal.classList.add('is-hidden');
-  ref.backdrop.classList.add('is-hidden');
+  console.log(ref.openModalBtn);
+  ref.modal.classList.toggle('is-hidden');
+  ref.backdrop.classList.toggle('is-hidden');
 };
 
 ref.openModalBtn.addEventListener('click', toggleModal);
-
+ref.closeModalBtn.addEventListener('click', toggleModal);
 const markup = data
   .map(
     ({ img, price, title }) =>
       `<div class="shop-item parent">
       <div class="shop-item-thumb">
         <img class="shop-item-img" src=${img} alt="shop image" />
-      </div> 
+      </div>
       <div class="flex shop-price">
         <p class="price">${price}</p>
         <div class="shop-cart"></div>
       </div>
       <div class="flex shop-content">
-        <h4 class="shop-item-title">${title}</h4> 
+        <h4 class="shop-item-title">${title}</h4>
         <div class="shop-star"></div>
       </div>
     </div>`
@@ -44,21 +44,11 @@ const markupComments = comments
       `<div class="comments-item ">
         <div class="shop-star-comments"></div>
         <p class="comments-content">${comment}</p>
-        <h4 class="comments-item-title">${title}</h4> 
+        <h4 class="comments-item-title">${title}</h4>
     </div>`
   )
   .join('');
-const spoylerOne = () => `
-<div class="spoylerDiv-one"> 
- <div class="spoyler-one">
-  <p >আমাদের পৃষ্ঠাটি আপনার অভিজ্ঞতা উন্নত করতে কুকিজ ব্যবহার করে। আপনি চাইলে তাদের নিষ্ক্রিয় করতে পারেন, কিন্তু এটি ওয়েবসাইটের কিছু কার্যক্ষমতা সীমিত করতে পারে। আমাদের কুকি নীতি দেখুন।</p>
-  <button class="hero-button spoyler-one">সব কুকিজকে গ্রহণ করুন</button>
-  <button class="hero-button spoyler-one">কুকিজের সেটিংস</button>
-</div>
-</div>
-  `;
-
-ref.spoylerOne.insertAdjacentHTML('afterbegin', spoylerOne);
 
 ref.shopDiv.insertAdjacentHTML('beforeend', markup);
+
 ref.commentsDiv.insertAdjacentHTML('beforeend', markupComments);
